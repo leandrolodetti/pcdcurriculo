ccdacc0c85693c73b4e8fd4843c6101ca9984557
leandrolodetti <49247775+leandrolodetti@users.noreply.github.com>
2019-06-21 15:58:11 -0300

    V2.0.34

--- mariaDB/Script pcdcurriculo.sql
+++ mariaDB/Script pcdcurriculo.sql
@@ -56,16 +56,17 @@ ENGINE = InnoDB;
 -- Table `pcdcurriculo`.`TiposDeficiencia`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pcdcurriculo`.`TiposDeficiencia` (
-  `idTiposDeficiencia` INT NOT NULL AUTO_INCREMENT,
+  `idTiposDeficiencia` INT NOT NULL,
   `tipo_deficiencia` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idTiposDeficiencia`))
 ENGINE = InnoDB;
 
-INSERT INTO TiposDeficiencia(tipo_deficiencia) VALUES('Auditiva');
-INSERT INTO TiposDeficiencia(tipo_deficiencia) VALUES('Fala');
-INSERT INTO TiposDeficiencia(tipo_deficiencia) VALUES('Física');
-INSERT INTO TiposDeficiencia(tipo_deficiencia) VALUES('Intelectual/Mental');
-INSERT INTO TiposDeficiencia(tipo_deficiencia) VALUES('Visual');
+INSERT INTO TiposDeficiencia(idTiposDeficiencia, tipo_deficiencia) VALUES(0,'Vazio');
+INSERT INTO TiposDeficiencia(idTiposDeficiencia, tipo_deficiencia) VALUES(1,'Auditiva');
+INSERT INTO TiposDeficiencia(idTiposDeficiencia, tipo_deficiencia) VALUES(2,'Fala');
+INSERT INTO TiposDeficiencia(idTiposDeficiencia, tipo_deficiencia) VALUES(3,'Física');
+INSERT INTO TiposDeficiencia(idTiposDeficiencia, tipo_deficiencia) VALUES(4,'Intelectual/Mental');
+INSERT INTO TiposDeficiencia(idTiposDeficiencia, tipo_deficiencia) VALUES(5,'Visual');
 
 
 -- -----------------------------------------------------
@@ -116,6 +117,129 @@ CREATE TABLE IF NOT EXISTS `pcdcurriculo`.`Categoria` (
 ENGINE = InnoDB;
 
 INSERT INTO Categoria(descricao) VALUES ('Administração Comercial/Vendas');
+INSERT INTO Categoria(descricao) VALUES ('Administração de Empresas');
+INSERT INTO Categoria(descricao) VALUES ('Administração Pública');
+INSERT INTO Categoria(descricao) VALUES ('Advocacia/Jurídica');
+INSERT INTO Categoria(descricao) VALUES ('Agronomia');
+INSERT INTO Categoria(descricao) VALUES ('Arquitetura/Urbanismo');
+INSERT INTO Categoria(descricao) VALUES ('Arquivologia');
+INSERT INTO Categoria(descricao) VALUES ('Artes Cênicas');
+INSERT INTO Categoria(descricao) VALUES ('Artes Gráficas');
+INSERT INTO Categoria(descricao) VALUES ('Artes Plásticas');
+INSERT INTO Categoria(descricao) VALUES ('Atend. a cliente - Bares e Restaurantes');
+INSERT INTO Categoria(descricao) VALUES ('Atend. a cliente - Telefonistas/Recepcionistas');
+INSERT INTO Categoria(descricao) VALUES ('Atend. a cliente - Telemarketing/Call Center');
+INSERT INTO Categoria(descricao) VALUES ('Atend. a cliente');
+INSERT INTO Categoria(descricao) VALUES ('Auditoria');
+INSERT INTO Categoria(descricao) VALUES ('Bancos');
+INSERT INTO Categoria(descricao) VALUES ('Biblioteconomia');
+INSERT INTO Categoria(descricao) VALUES ('Biologia/Ciências Biológicas');
+INSERT INTO Categoria(descricao) VALUES ('Biomedicina');
+INSERT INTO Categoria(descricao) VALUES ('Ciências Sociais');
+INSERT INTO Categoria(descricao) VALUES ('Cinema');
+INSERT INTO Categoria(descricao) VALUES ('Comércio Exterior');
+INSERT INTO Categoria(descricao) VALUES ('Compras');
+INSERT INTO Categoria(descricao) VALUES ('Comunicação');
+INSERT INTO Categoria(descricao) VALUES ('Construção Civil');
+INSERT INTO Categoria(descricao) VALUES ('Contabilidade');
+INSERT INTO Categoria(descricao) VALUES ('Controladoria');
+INSERT INTO Categoria(descricao) VALUES ('Culinária/Gastronomia');
+INSERT INTO Categoria(descricao) VALUES ('Desenho Industrial');
+INSERT INTO Categoria(descricao) VALUES ('Design de Interiores');
+INSERT INTO Categoria(descricao) VALUES ('Design Gráfico');
+INSERT INTO Categoria(descricao) VALUES ('Ecologia/Meio Ambiente');
+INSERT INTO Categoria(descricao) VALUES ('Economia');
+INSERT INTO Categoria(descricao) VALUES ('Enfermagem');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia de Alimentos');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia Civil');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia Eletrônica');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia Eletrotécnica');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia Mecânica');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia Metalúrgica e de Materiais');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia de Minas');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia de Produção');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia Química');
+INSERT INTO Categoria(descricao) VALUES ('Engenharia - Outras');
+INSERT INTO Categoria(descricao) VALUES ('Ensino Superior e Pesquisa');
+INSERT INTO Categoria(descricao) VALUES ('Ensino - Outros');
+INSERT INTO Categoria(descricao) VALUES ('Entretenimento');
+INSERT INTO Categoria(descricao) VALUES ('Esportes');
+INSERT INTO Categoria(descricao) VALUES ('Estética');
+INSERT INTO Categoria(descricao) VALUES ('Farmácia');
+INSERT INTO Categoria(descricao) VALUES ('Filosofia');
+INSERT INTO Categoria(descricao) VALUES ('Finanças');
+INSERT INTO Categoria(descricao) VALUES ('Fiscal');
+INSERT INTO Categoria(descricao) VALUES ('Física');
+INSERT INTO Categoria(descricao) VALUES ('Fisioterapia');
+INSERT INTO Categoria(descricao) VALUES ('Fonoaudiologia');
+INSERT INTO Categoria(descricao) VALUES ('Geografia');
+INSERT INTO Categoria(descricao) VALUES ('Geologia');
+INSERT INTO Categoria(descricao) VALUES ('Gestão Empresarial');
+INSERT INTO Categoria(descricao) VALUES ('História');
+INSERT INTO Categoria(descricao) VALUES ('Hotelaria');
+INSERT INTO Categoria(descricao) VALUES ('Informática/T.I.');
+INSERT INTO Categoria(descricao) VALUES ('Internet');
+INSERT INTO Categoria(descricao) VALUES ('Jornalismo');
+INSERT INTO Categoria(descricao) VALUES ('Letras');
+INSERT INTO Categoria(descricao) VALUES ('Logística');
+INSERT INTO Categoria(descricao) VALUES ('Manutenção Industrial');
+INSERT INTO Categoria(descricao) VALUES ('Manutenção Predial');
+INSERT INTO Categoria(descricao) VALUES ('Marketing');
+INSERT INTO Categoria(descricao) VALUES ('Matemática/Estatística');
+INSERT INTO Categoria(descricao) VALUES ('Medicina/Hospitalar');
+INSERT INTO Categoria(descricao) VALUES ('Meteorologia');
+INSERT INTO Categoria(descricao) VALUES ('Mineração');
+INSERT INTO Categoria(descricao) VALUES ('Moda');
+INSERT INTO Categoria(descricao) VALUES ('Museologia');
+INSERT INTO Categoria(descricao) VALUES ('Música');
+INSERT INTO Categoria(descricao) VALUES ('Nutrição');
+INSERT INTO Categoria(descricao) VALUES ('Oceanografia');
+INSERT INTO Categoria(descricao) VALUES ('Odontologia');
+INSERT INTO Categoria(descricao) VALUES ('Organização de Eventos/Produção Cultural');
+INSERT INTO Categoria(descricao) VALUES ('Organização e Métodos');
+INSERT INTO Categoria(descricao) VALUES ('Pesquisa de Mercado');
+INSERT INTO Categoria(descricao) VALUES ('Petrolífera');
+INSERT INTO Categoria(descricao) VALUES ('Produção/Fabricação');
+INSERT INTO Categoria(descricao) VALUES ('Propaganda');
+INSERT INTO Categoria(descricao) VALUES ('Psicologia');
+INSERT INTO Categoria(descricao) VALUES ('Qualidade');
+INSERT INTO Categoria(descricao) VALUES ('Química');
+INSERT INTO Categoria(descricao) VALUES ('Radialismo e Televisão');
+INSERT INTO Categoria(descricao) VALUES ('Recursos Humanos');
+INSERT INTO Categoria(descricao) VALUES ('Relações Internacionais');
+INSERT INTO Categoria(descricao) VALUES ('Relações Públicas');
+INSERT INTO Categoria(descricao) VALUES ('Secretariado');
+INSERT INTO Categoria(descricao) VALUES ('Segurança e Saúde no Trabalho');
+INSERT INTO Categoria(descricao) VALUES ('Segurança Patrimonial');
+INSERT INTO Categoria(descricao) VALUES ('Seguros');
+INSERT INTO Categoria(descricao) VALUES ('Serviço Social');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Administrativos');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Domésticos');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Especializados - Açougue');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Especializados - Padaria/Confeitaria');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Especializados - Peixaria');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Gerais');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Técnicos - Elétricos');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Técnicos - Eletrônicos');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Técnicos - Mecânicos');
+INSERT INTO Categoria(descricao) VALUES ('Serviços Técnicos - Outros');
+INSERT INTO Categoria(descricao) VALUES ('Suprimentos');
+INSERT INTO Categoria(descricao) VALUES ('Telecomunicações');
+INSERT INTO Categoria(descricao) VALUES ('Terapia Ocupacional');
+INSERT INTO Categoria(descricao) VALUES ('Terceiro Setor/Responsabilidade Social');
+INSERT INTO Categoria(descricao) VALUES ('Tradução/Interpretação');
+INSERT INTO Categoria(descricao) VALUES ('Transporte Aéreo');
+INSERT INTO Categoria(descricao) VALUES ('Transporte Marítimo');
+INSERT INTO Categoria(descricao) VALUES ('Transporte Terrestre');
+INSERT INTO Categoria(descricao) VALUES ('Tributária');
+INSERT INTO Categoria(descricao) VALUES ('Turismo');
+INSERT INTO Categoria(descricao) VALUES ('Vendas');
+INSERT INTO Categoria(descricao) VALUES ('Vendas - Varejo');
+INSERT INTO Categoria(descricao) VALUES ('Vendas Técnicas');
+INSERT INTO Categoria(descricao) VALUES ('Veterinária');
+INSERT INTO Categoria(descricao) VALUES ('Web Design');
+INSERT INTO Categoria(descricao) VALUES ('Zoologia');
+INSERT INTO Categoria(descricao) VALUES ('Zootecnia');
 
 -- -----------------------------------------------------
 -- Table `pcdcurriculo`.`Nivel`
@@ -212,6 +336,36 @@ CREATE TABLE IF NOT EXISTS `pcdcurriculo`.`RestricaoDeficiencia` (
 )
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `pcdcurriculo`.`RecuperaLogin`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pcdcurriculo`.`RecuperaLogin` (
+  `idRecuperaLogin` INT NOT NULL AUTO_INCREMENT,
+  `token` VARCHAR(255) NOT NULL,
+  `data_criacao` DATETIME NOT NULL,
+  `Empresa_idEmpresa` INT NULL,
+  `Candidato_idCandidato` INT NULL,
+  PRIMARY KEY (`idRecuperaLogin`),
+  UNIQUE INDEX `Candidato_idCandidato_UNIQUE` (`Candidato_idCandidato` ASC),
+  UNIQUE INDEX `Empresa_idEmpresa_UNIQUE` (`Empresa_idEmpresa` ASC),
+    FOREIGN KEY (`Empresa_idEmpresa`) REFERENCES `pcdcurriculo`.`Empresa` (`idEmpresa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (`Candidato_idCandidato`) REFERENCES `pcdcurriculo`.`Candidato` (`idCandidato`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `pcdcurriculo`.`DisparaEmail`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pcdcurriculo`.`DisparaEmail` (
+  `idDisparaEmail` INT NOT NULL AUTO_INCREMENT,
+  `email_candidato` VARCHAR(45) NOT NULL,
+  `titulo_vaga` VARCHAR(60) NOT NULL,
+  `Vaga_idVaga` INT NOT NULL,
+  PRIMARY KEY (`idDisparaEmail`),
+  UNIQUE INDEX `email_candidato_UNIQUE` (`email_candidato` ASC),
+    FOREIGN KEY (`Vaga_idVaga`) REFERENCES `pcdcurriculo`.`Vaga` (`idVaga`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = InnoDB;
 
 CREATE USER 'publico'@'localhost' IDENTIFIED BY 'ede02e60172b237f72e48299bce8ac8e';
 GRANT ALL PRIVILEGES ON pcdcurriculo.* TO 'publico'@'localhost';
